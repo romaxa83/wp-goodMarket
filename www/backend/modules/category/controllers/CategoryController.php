485ed1cb10d3f09b9ff8a491ca6bfbcb0274f9fc
Vladislav Shk <vlad.shkrabak@bws.pp.ua>
2019-08-28 10:43:53 +0300

    CRUD for category added

--- www/backend/modules/category/controllers/CategoryController.php
+++ www/backend/modules/category/controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace backend\modules\category\controllers;
 
 use backend\modules\category\models\CategoryLang;
+use backend\modules\seo\models\SeoMeta;
 use common\models\Lang;
 use Yii;
 use backend\modules\category\models\Category;
@@ -10,6 +11,7 @@ use backend\controllers\BaseController;
 use backend\widgets\SeoWidget;
 use common\controllers\AccessController;
 use yii\filters\AccessControl;
+use yii\helpers\ArrayHelper;
 use yii\web\ForbiddenHttpException;
 /**
  * MenuController реализует CRUD-систему для модели Menu.
@@ -51,9 +53,32 @@ class CategoryController extends BaseController {
         ]);
     }
 
+    public function actionCreate() {
+        $model = new Category();
+        $model->scenario = Category::ADDED_CATEGORY;
+        if (($post = Yii::$app->request->post()) && $model->load($post)) {
+            $transaction = Yii::$app->db->beginTransaction();
+            try {
+                if ($model->save() && CategoryLang::saveAll($model->id, $post['Category']['Language']) && SeoWidget::save($model->id, 'category', $post['SEO']) > 0) {
+                    $transaction->commit();
+                    Yii::$app->session->setFlash('success', 'Пункт успешно добавлен');
+                    return $this->redirect([$post['save']]);
+                }
+            } catch (\Exception $e) {
+                $transaction->rollBack();
+                throw $e;
+            } catch (\Throwable $e) {
+                $transaction->rollBack();
+                throw $e;
+            }
+        }
+        return $this->render('form-category', ['model' => $model, 'categoryList' => $model->getSelect2List()]);
+    }
+
     public function actionEdit() {
         $id = Yii::$app->request->get('id');
         $model = Category::find()->where(['id' => $id])->with('categoryLang')->with('categoryLang.lang')->one();
+        $model->scenario = Category::SAVED_CATEGORY;
 
         $data['Language'] = [];
         foreach ($model->categoryLang as $v) {
@@ -63,19 +88,9 @@ class CategoryController extends BaseController {
         }
         $model->languageData = $data;
 
-        $lang_id = Lang::find()->select('id')->where(['alias' => Yii::$app->params['settings']['mainContentLanguage']])->one()->id;
-        $parent_name = CategoryLang::find()->select('name')->where(['category_id' => $model->parent_id, 'lang_id' => $lang_id])->one();
-        $parent_name = isset($parent_name->name) ? $parent_name->name : 'Нет';
-
-        if ($post = Yii::$app->request->post()){
+        if (($post = Yii::$app->request->post()) && $model->load($post)){
             $transaction = Yii::$app->db->beginTransaction();
             try {
-                $model->scenario = Category::SAVED_CATEGORY;
-                $model->rating = $post['Category']['rating'];
-                $model->media_id = $post['Category']['media_id'];
-                $model->publish = $post['Category']['publish'];
-                $model->alias = $post['Category']['alias'];
-
                 if ($model->save() && CategoryLang::saveAll($model->id, $post['Category']['Language']) && SeoWidget::save($id, 'category', $post['SEO']) > 0) {
                     $transaction->commit();
                     Yii::$app->session->setFlash('success', 'Категория успешно отредактирована');
@@ -91,8 +106,27 @@ class CategoryController extends BaseController {
         }
         return $this->render('form-category', [
             'model' => $model,
-            'parent_name' => $parent_name
+            'categoryList' => $model->getSelect2List()
         ]);
+    }
+
+    public function actionDelete() {
+        $id = Yii::$app->request->get('id');
+        $transaction = Yii::$app->db->beginTransaction();
+        try {
+            Category::deleteAll('id = :id', ['id' => $id]);
+            SeoMeta::deleteAll(['page_id' => $id, 'alias' => 'category']);
+            CategoryLang::deleteAll(['category_id' => $id]);
+            $transaction->commit();
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            throw $e;
+        } catch (\Throwable $e) {
+            $transaction->rollBack();
+            throw $e;
+        }
+        Yii::$app->session->setFlash('success', 'Пункт успешно удален');
+        $this->redirect(['/category/category']);
     }
 
     public function actionUpdateStatus() {
