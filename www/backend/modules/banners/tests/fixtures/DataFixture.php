0b83fb3efdf06fc6d5489132dee352691ccd7fb2
nick <nick@localhost>
2019-09-04 16:21:41 +0300

    update test banner

---
+++ www/backend/modules/banners/tests/fixtures/DataFixture.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace backend\modules\banners\tests\fixtures;
+
+use yii\test\ArrayFixture;
+
+class DataFixture extends ArrayFixture {
+
+    public $dataFile = 'backend/modules/banners/tests/_data/data.php';
+
+}
